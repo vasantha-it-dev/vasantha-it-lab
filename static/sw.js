0cb14fb1e192dfa166cb3e192a41cb5d22dbc8ef
vasanth kumar <challameenalchallameenal@gmail.com>
2026-03-26 22:53:00 +0530

    Engine update for Play Store

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,19 @@
-// This is a basic Service Worker to pass Play Store requirements
+// This is the Vasantha IT Lab Service Worker
+const CACHE_NAME = 'vasantha-lab-v1';
+
+self.addEventListener('install', (event) => {
+  console.log('Vasantha Lab Service Worker: Installed');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('Vasantha Lab Service Worker: Activated');
+});
+
+// This is the part that Play Store requires for "Offline" support
 self.addEventListener('fetch', (event) => {
-  // It doesn't need to do much for now, just exist!
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response("You are offline, but Vasantha Lab is still here!");
+    })
+  );
 });
