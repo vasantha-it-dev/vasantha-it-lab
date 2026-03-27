56d489af5fe65fc5341260cc1238b71a621a41bc
vasanth kumar <challameenalchallameenal@gmail.com>
2026-03-27 18:22:29 +0530

    Fixed template paths for white screen

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, send_from_directory, make_response
 import os
 
-app = Flask(__name__)
+# This tells Flask to look inside the master1 folder for your files
+app = Flask(__name__, 
+            template_folder='templates', 
+            static_folder='static')
 
-# Serve the PWA manifest directly from the root
 @app.route('/manifest.json')
 def serve_manifest():
     response = make_response(send_from_directory('.', 'manifest.json'))
     response.headers['Content-Type'] = 'application/json'
     return response
 
-# Serve the Service Worker directly from the root
 @app.route('/sw.js')
 def serve_sw():
     response = make_response(send_from_directory('.', 'sw.js'))
     response.headers['Content-Type'] = 'application/javascript'
     return response
 
-# Serve main app routes
 @app.route('/')
 def home():
     return render_template('portfolio.html')
