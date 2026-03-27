a4fe74ed1fdd8cb4ab94573a08e8ef1c538c8f86
vasanth kumar <challameenalchallameenal@gmail.com>
2026-03-27 17:59:54 +0530

    Final professional icon update

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
-from flask import Flask, render_template, send_from_directory
+from Flask import Flask, render_template, send_from_directory, make_response
 import os
 
 app = Flask(__name__)
 
-# This serves ANY file in your main folder (sw.js, manifest.json, etc.)
-@app.route('/<path:filename>')
-def serve_root_files(filename):
-    return send_from_directory('.', filename)
+# Serve the PWA manifest directly from the root
+@app.route('/manifest.json')
+def serve_manifest():
+    response = make_response(send_from_directory('.', 'manifest.json'))
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
+# Serve the Service Worker directly from the root
+@app.route('/sw.js')
+def serve_sw():
+    response = make_response(send_from_directory('.', 'sw.js'))
+    response.headers['Content-Type'] = 'application/javascript'
+    return response
+
+# Serve main app routes
 @app.route('/')
 def home():
     return render_template('portfolio.html')
-
-@app.route('/builder')
-def builder():
-    return render_template('builder.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
